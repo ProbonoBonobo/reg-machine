25a75437f472d05ee536cba75a1ca09eba5cc099
ProbonoBonobo <kzeidler@berkeley.edu>
2015-11-05 05:21:55 -0800

    initial landing page commit v2

--- src/cljs/josefk/core.cljs
+++ src/cljs/josefk/core.cljs
@@ -430,7 +430,6 @@
                               (d/tr
                                 (d/td (d/code {} "Shift key:"))
                                 (d/td (d/code {} (str (get-in @app-state [:shift-in])))))))
-
                      ]))))
 
     (def stringnums (set ["1" "2" "3" "4" "5" "6" "7" "8" "9" "0"]))
